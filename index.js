25cb775f93ce8e2b409d5a70df28f949dc5b3db0
Azat Ahmedov <payalord@gmail.com>
2022-04-08 11:22:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,19 @@
  * Copyright (c) 2022 Azat Ahmedov <payalord@gmail.com>
  * MIT License
  */
-const command = process.argv.find(e => e.includes(process.platform))?.split('=')[1]
-if (command) {
-    const { exec } = require('child_process')
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(error)
-            return
-        }
-        if (stdout.length) console.log(stdout)
-        if (stderr.length) console.error(stderr)
-    })
+function crossExec(args, options = {}) {
+    const command = args.find(e => e.includes(process.platform))?.split('=')[1]
+    if (command) {
+        const { exec } = require('child_process')
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(error)
+                return
+            }
+            if (stdout.length) console.log(stdout)
+            if (stderr.length) console.error(stderr)
+        })
+    }
 }
+
+module.exports = crossExec
